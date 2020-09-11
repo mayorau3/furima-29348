5d8027e303c4fd933c116f419c28f96e69632c65
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-11 19:12:10 +0900

    devise_parameter_sanitizerでカラム許容

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :kanji_family_name, :kanji_first_name, :kana_family_name, :kana_first_name, :birth_date])
+  end
 
   private
 
