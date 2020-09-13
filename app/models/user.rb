ad878494857964190008d1af4968512fd846ec44
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-13 16:18:05 +0900

    ユーザー情報バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # deviseのバリデーションは上記で設定されている。
+  validates :nickname, :birth_date, presence: true
+
+  # パスワード半角英数字混合
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  # 名前漢字・平仮名・カタカナ
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :kanji_family_name
+    validates :kanji_first_name
+  end
+
+    # 名前カタカナ
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :kana_family_name
+    validates :kana_first_name
+  end
 end
