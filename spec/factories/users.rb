859236b2ac65a5eb963805b1b7ccc2817c92c966
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-14 16:51:28 +0900

    Rubocop実行後、check内容自動修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 8)
-    password {password}
-    password_confirmation {password}
-    kanji_family_name {"名字かんじゼンカク"}
-    kanji_first_name {"名まえテスト"}
-    kana_family_name {"ミョウジ"}
-    kana_first_name {"ナマエ"}
-    birth_date {"2000-12-31"}
+    password { password }
+    password_confirmation { password }
+    kanji_family_name { '名字かんじゼンカク' }
+    kanji_first_name { '名まえテスト' }
+    kana_family_name { 'ミョウジ' }
+    kana_first_name { 'ナマエ' }
+    birth_date { '2000-12-31' }
   end
 end
