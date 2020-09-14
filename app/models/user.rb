b13fe0841bf9a0948b7ad69456e5a0f3606ec89e
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-14 11:11:20 +0900

    エラーメッセージ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
 
   # パスワード半角英数字混合
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, message:"Include both letters and numbers"
 
   # 名前漢字・平仮名・カタカナ
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:"Full-width characters" } do
     validates :kanji_family_name
     validates :kanji_first_name
   end
 
     # 名前カタカナ
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message:"Full-width katakana characters" } do
     validates :kana_family_name
     validates :kana_first_name
   end
