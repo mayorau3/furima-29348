b63213377530602af57fcf771e7c9af78100b9ca
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-14 14:42:32 +0900

    FactoryBot作成（テストデータ生成）

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 8)
+    password {password}
+    password_confirmation {password}
+    kanji_family_name {"名字かんじ"}
+    kanji_first_name {"名まえ"}
+    kana_family_name {"ミョウジ"}
+    kana_first_name {"ナマエ"}
+    birth_date {"2000-12-31"}
+  end
+end
