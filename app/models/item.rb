32cb596548f55bb858354f9b0183bf2ca6f882de
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-15 21:42:42 +0900

    画像ストロングパラメーター、バリデーション、ビュー

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
@@ -8,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_from
   belongs_to_active_hash :num_day
 
-  validates :name, :explanation, :category_id, :status_id, :ship_fee_id, :ship_from_id, :num_day_id, presence: true
+  validates :name, :explanation, :category_id, :status_id, :ship_fee_id, :ship_from_id, :num_day_id, :image, presence: true
 
   # 価格の範囲指定 ¥300〜¥9,999,999の間、半角数字のみ可
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
