c9d681a4ee013c14c4b0c4a664a05ba5830b3ba7
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-15 12:42:21 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :name, :explanation, :category, :item_status, :shipping_fee, :ship_from, :num_days, presence: true
+
+  # 価格の範囲指定 ¥300〜¥9,999,999の間、半角数字のみ可
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
