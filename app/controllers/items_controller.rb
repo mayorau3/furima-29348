d343f247926d00f6fcd0f50da3255ed766242722
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-15 19:10:07 +0900

    コントローラー作成、ビューへのインスタンス変数引き渡し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,17 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :ship_fee_id, :ship_from_id, :num_day_id, :price)
+  end
 end
