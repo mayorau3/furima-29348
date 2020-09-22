e401cd2bad092e7dff1fb1fc9e16668c250600b1
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-22 12:20:30 +0900

    テーブル作成、DBへ反映

--- db/migrate/20200922023518_create_addresses.rb
+++ db/migrate/20200922023518_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :zip_code,     null: false
+      t.integer :prefecture,  null: false
+      t.string :city,         null: false
+      t.string :address1,     null: false
+      t.string :address2
+      t.string :phone_num,    null: false
+      t.references :purchase, null: false, foreign_key: true
       t.timestamps
     end
   end
