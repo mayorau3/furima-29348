5898adbd6d56ab9295592a093e08e33a41bf2b77
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-23 19:10:52 +0900

    ログインしていない場合の画面遷移

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,6 @@
 class PurchasesController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def index
     @item = Item.find(params[:item_id])
     @purchase = PurchaseAddress.new
