bf0868c7b88c1d73b7fd881d674a53a8f60a8d1d
mayorau3 <mayo1129rau1206@gmail.com>
2020-09-23 18:54:19 +0900

    クレジットカード決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,13 +6,33 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase = PurchaseAddress.new(purchase_add_params)
-    @purchase.save
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
   
   private
 
   def purchase_add_params
     params.require(:purchase_address).permit(:zip_code, :prefecture, :city, :address1, :address2, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def card_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: card_params[:token],
+      currency:'jpy'
+    )
   end
 end
